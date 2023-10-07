3bdac062e9701b164c12d8125d605ecf70c7c41f
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2023-10-07 20:54:26 -0300

    Add files via upload

---
+++ aula16ex1.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+using namespace std;
+#include "data.cpp"
+
+struct Automovel{
+    string modelo;
+    char marca[40];
+    Data dtAquisicao
+}
+
