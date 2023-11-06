9b63c712fd2a94cd26843b3a5521fa6a0f91f246
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2023-11-06 20:46:22 -0300

    Add files via upload

--- aula9_while_for.cpp
+++ aula9_while_for.cpp
@@ -19,9 +19,12 @@ int main(){
 }
 */
 int main(){
-    int x, y;
-    for(x=0, y=1 ; x<=10; x++, y+=2){
-        cout << "OBABOXX - " << x << " - "<< y << "\n";
+    int x, y, z;
+
+    for(int tmp=0; tmp < 10000000 ; tmp++);
+
+    for(x=0, y=1, z=0 ; x<=10 && z<=6; x++, y+=2, z+=2){
+        cout << "OBABOXX - " << x << " - "<< y <<" - "<< z << "\n";
     }
 
     return 0;
