3aa63304218ee7a63162bebf0801d10cedb619cd
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2025-09-10 22:08:28 -0300

    Create automato.c

---
+++ automato.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <cstdio>
+#include <conio.h>
+
+S' -> R#
+R
+char buffer[256] = "(a+b)*.a.b#";
+int pos=0;
+
+char prox() {
+    return buffer[pos++];
+}
+
+void Accept(token_esperado, string msg)
+{
+    if(token==esperado)
+        token = prox();
+        
+}
+void R() {
+    if ()
+}
+void S {
+    if(token='+' or token='.') {
+        token = prox();
+        
+    }
+}
+int main()
+{
+    token    
+
+    return 0;
+}
